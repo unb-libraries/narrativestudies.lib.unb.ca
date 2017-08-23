7e13e406deca067d2b32845e5db437367957b1e1
Camilo Villamizar <camilocodes@gmail.com>
2017-08-23 15:51:53 -0300

    NS-12 Add search label block and other layout

---
+++ custom/modules/ns_navigation/src/Plugin/Block/SearchLabel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\ns_navigation\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a title block for the exposed global search form.
+ *
+ * @Block(
+ *   id = "search_label",
+ *   admin_label = @Translation("Search Label"),
+ *   category = @Translation("Misc"),
+ * )
+ */
+class SearchLabel extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $text = '<b>Search more than 16,000 items from the blibliography, ' .
+      'including articles, books, essays, and videos.</b>';
+
+    return [
+        '#markup' => $this->t($text),
+    ];
+  }
+
+}

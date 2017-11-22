e841b4606df939416dd721d32ee8167f6758e583
Jacob Sanford <jsanford@unb.ca>
2017-11-22 08:52:19 -0400

    Update visual regression reference images for dev

---
+++ tests/backstop/dev/backstop_data/html_report/config.js
@@ -0,0 +1,44 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20171122-125212/narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_0_phone.png",
+        "label": "dev-narrativestudies.lib.unb.ca Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20171122-125212/narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
+        "label": "dev-narrativestudies.lib.unb.ca Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
+        "test": "../bitmaps_test/20171122-125212/narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
+        "selector": "document",
+        "fileName": "narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
+        "label": "dev-narrativestudies.lib.unb.ca Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/narrativestudies_lib_unb_ca_dev-narrativestudieslibunbca_Homepage_0_document_2_monitor.png"
+      },
+      "status": "fail"
+    }
+  ]
+});

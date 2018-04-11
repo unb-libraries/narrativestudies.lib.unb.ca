c8b512bb61d1696e841ce3dd935f8a5bc815c35d
Camilo Villamizar <cvillami@unb.ca>
2018-04-11 15:27:02 -0300

    Update visual regression reference images for prod

--- tests/backstop/prod/backstop_data/html_report/config.js
+++ tests/backstop/prod/backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180410-195710/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180411-182650/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png",
         "label": "narrativestudies.lib.unb.ca Homepage",
@@ -14,20 +14,20 @@ report({
           "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": -80
+            "height": -39
           },
-          "misMatchPercentage": "8.74",
-          "analysisTime": 120,
+          "misMatchPercentage": "7.32",
+          "analysisTime": 130,
           "getDiffImage": null
         },
-        "diffImage": "../bitmaps_test/20180410-195710/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png"
+        "diffImage": "../bitmaps_test/20180411-182650/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20180410-195710/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180411-182650/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
         "label": "narrativestudies.lib.unb.ca Homepage",
@@ -37,20 +37,20 @@ report({
           "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": -40
+            "height": -15
           },
-          "misMatchPercentage": "3.02",
-          "analysisTime": 221,
+          "misMatchPercentage": "3.73",
+          "analysisTime": 204,
           "getDiffImage": null
         },
-        "diffImage": "../bitmaps_test/20180410-195710/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png"
+        "diffImage": "../bitmaps_test/20180411-182650/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
-        "test": "../bitmaps_test/20180410-195710/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
+        "test": "../bitmaps_test/20180411-182650/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
         "selector": "document",
         "fileName": "narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
         "label": "narrativestudies.lib.unb.ca Homepage",
@@ -60,13 +60,13 @@ report({
           "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": -30
+            "height": -18
           },
-          "misMatchPercentage": "1.75",
-          "analysisTime": 398,
+          "misMatchPercentage": "2.04",
+          "analysisTime": 324,
           "getDiffImage": null
         },
-        "diffImage": "../bitmaps_test/20180410-195710/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png"
+        "diffImage": "../bitmaps_test/20180411-182650/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png"
       },
       "status": "fail"
     }

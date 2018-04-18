e379e7b37a3b26ad6f7ddc1881241245b20dd9ff
Camilo Villamizar <cvillami@unb.ca>
2018-04-18 10:49:44 -0300

    Update visual regression reference images for prod

--- tests/backstop/prod/backstop_data/html_report/config.js
+++ tests/backstop/prod/backstop_data/html_report/config.js
@@ -4,30 +4,28 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180417-200223/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180418-134040/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png",
         "label": "narrativestudies.lib.unb.ca Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
-            "height": -20
+            "height": 0
           },
-          "misMatchPercentage": "7.29",
-          "analysisTime": 118,
+          "misMatchPercentage": "0.00",
           "getDiffImage": null
-        },
-        "diffImage": "../bitmaps_test/20180417-200223/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_0_phone.png"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20180417-200223/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180418-134040/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png",
         "label": "narrativestudies.lib.unb.ca Homepage",
@@ -39,18 +37,18 @@ report({
             "width": 0,
             "height": -10
           },
-          "misMatchPercentage": "9.94",
-          "analysisTime": 231,
+          "misMatchPercentage": "9.85",
+          "analysisTime": 151,
           "getDiffImage": null
         },
-        "diffImage": "../bitmaps_test/20180417-200223/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png"
+        "diffImage": "../bitmaps_test/20180418-134040/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_1_tablet.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
-        "test": "../bitmaps_test/20180417-200223/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
+        "test": "../bitmaps_test/20180418-134040/narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
         "selector": "document",
         "fileName": "narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png",
         "label": "narrativestudies.lib.unb.ca Homepage",
@@ -62,11 +60,11 @@ report({
             "width": 0,
             "height": -10
           },
-          "misMatchPercentage": "5.38",
-          "analysisTime": 377,
+          "misMatchPercentage": "5.33",
+          "analysisTime": 406,
           "getDiffImage": null
         },
-        "diffImage": "../bitmaps_test/20180417-200223/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png"
+        "diffImage": "../bitmaps_test/20180418-134040/failed_diff_narrativestudies_lib_unb_ca_narrativestudieslibunbca_Homepage_0_document_2_monitor.png"
       },
       "status": "fail"
     }

0fd2c013eb7a20cf131edd2ea64e410439f1a599
Jacob Sanford <jsanford@unb.ca>
2021-05-11 14:27:53 -0300

    Code standards fixes

--- custom/modules/ns_scripts/bib_clr.php
+++ custom/modules/ns_scripts/bib_clr.php
@@ -10,12 +10,12 @@ ini_set('memory_limit', '1024M');
 
 $entities = ['keyword', 'contributor', 'reference'];
 
-foreach($entities as $type) {
+foreach ($entities as $type) {
   $storage = \Drupal::entityTypeManager()->getStorage("bibcite_{$type}");
-  while(1) {
+  while (1) {
     $ids = $storage->getQuery()->range(0, 100)->execute();
     print "{$type}: " . count($ids) . "\n";
-    if(empty($ids)) {
+    if (empty($ids)) {
       break;
     }
     $storage->delete($storage->loadMultiple($ids));

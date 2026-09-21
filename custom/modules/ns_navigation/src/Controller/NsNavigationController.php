380aa4feeff9e4b3d2497171578f825ad67ccb4e
Camilo Villamizar <cvillami@unb.ca>
2026-09-21 12:01:10 -0300

    NS-47 Add CSV download functionality

--- custom/modules/ns_navigation/src/Controller/NsNavigationController.php
+++ custom/modules/ns_navigation/src/Controller/NsNavigationController.php
@@ -4,7 +4,10 @@ namespace Drupal\ns_navigation\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Routing\TrustedRedirectResponse;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Controller for Narrative Studies navigation.
@@ -38,6 +41,29 @@ class NsNavigationController extends ControllerBase {
       '#attributes' => [],
     ];
     return $element;
+  }
+
+  /**
+   * Serves the pre-generated export of all references as a download.
+   *
+   * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+   *   The CSV export, sent as an attachment.
+   */
+  public function downloadReferences() {
+    $file = ns_navigation_reference_export_file();
+
+    if (!$file) {
+      throw new NotFoundHttpException();
+    }
+
+    $response = new BinaryFileResponse($file);
+    $response->headers->set('Content-Type', 'text/csv');
+    $response->setContentDisposition(
+      ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+      'narrative-studies-references.csv'
+    );
+
+    return $response;
   }
 
   /**
